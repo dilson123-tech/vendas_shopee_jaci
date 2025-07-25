0c427214bcb9b8ccb4d826993a38c08872d1d6d7
Dilson Pereira <dilsonpereira231@gmail.com>
2025-07-25 02:54:26 -0300

    Atualiza links de Instagram e TikTok e finaliza layout

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   console.log("Achadinhos da Jaci está no ar!");
 });
-<script>
-  function ampliarImagem(img) {
-    const modal = document.createElement('img');
-    modal.src = img.src;
-    modal.classList.add('modal-img');
-    modal.onclick = () => modal.remove();
-    document.body.appendChild(modal);
-  }
-</script>
+
+function ampliarImagem(img) {
+  const modal = document.createElement("img");
+  modal.src = img.src;
+  modal.classList.add("modal-img");
+  modal.onclick = () => modal.remove();
+  document.body.appendChild(modal);
+}
+
+const swiper = new Swiper(".mySwiper", {
+  direction: "horizontal",
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
+
+function trocarImagem(src) {
+  document.getElementById("imagem-principal").src = src;
+}
